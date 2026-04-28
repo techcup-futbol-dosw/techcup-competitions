263583025dd3281f4808556071f5db39b35a88da
JuliRodC <majuliroca@gmail.com>
2026-04-27 21:38:29 -0500

    Se arregla el test

--- src/test/java/edu/eci/dosw/competitions/AppTest.java
+++ src/test/java/edu/eci/dosw/competitions/AppTest.java
@@ -1,11 +1,7 @@
 package edu.eci.dosw.competitions;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class AppTest {
 
     @Test
