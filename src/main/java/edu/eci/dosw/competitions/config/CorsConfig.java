eae07a3fcde9fa36ea00e0538cb4d1e42177bcac
JuliRodC <majuliroca@gmail.com>
2026-05-20 22:07:47 -0500

    Agregar cambio pequeño para funcion del front

--- src/main/java/edu/eci/dosw/competitions/config/CorsConfig.java
+++ src/main/java/edu/eci/dosw/competitions/config/CorsConfig.java
@@ -14,10 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://techcup-front.vercel.app"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
